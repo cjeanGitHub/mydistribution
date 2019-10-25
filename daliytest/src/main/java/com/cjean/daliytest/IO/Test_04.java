e808a27026bc023c301e7d277f8cb16f8bd385f2
chu_dliang <1417261875@qq.com>
2019-10-25 09:27:33 +0800

    完善方法

--- daliytest/src/main/java/com/cjean/daliytest/IO/Test_04.java
+++ daliytest/src/main/java/com/cjean/daliytest/IO/Test_04.java
@@ -2,7 +2,7 @@ package com.cjean.daliytest.IO;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -18,16 +18,25 @@ public class Test_04 {
 	 */
 	public void readFile(File file) {
 		if(file.isFile()) {
-			try {
-				FileInputStream fileInputStream = new FileInputStream(file);
+			try(FileInputStream fis = new FileInputStream(file);) {
 				int length = (int)file.length();
 				byte[] b = new byte[length];
-				int read = fileInputStream.read(b);
 				int len = 0;
-				while() {
-					
-				}
+				StringBuffer sbf = new StringBuffer();
 				
+				System.out.println(file.getParent()+"\\copyFile.txt");
+				File copyFile = new File(file.getParent()+"\\copyFile.txt");
+				FileOutputStream fos = new FileOutputStream(copyFile);
+				while( (len = fis.read(b)) != -1) {
+					//将文件内容写到内存中
+					sbf.append(new String(b,0,len));
+					//将内存中的文件内容写到 io文件中
+					fos.write(b, 0, len);
+					fos.flush();
+				}
+				fos.close();
+				//将内存中的文件内容写到控制台中
+				System.out.println(sbf.toString());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -36,7 +45,11 @@ public class Test_04 {
 	}
 
 	public static void main(String[] args) {
-
+		Test_04 test_04 = new Test_04();
+		File file = new File("D:\\workspaces\\distribution\\daliytest\\src\\main\\java\\com\\cjean\\daliytest\\IO\\Test_04.java");
+		System.out.println("开始");
+		test_04.readFile(file);
+		System.out.println("结束");
 	}
 
 }
